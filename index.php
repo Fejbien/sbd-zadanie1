35bc7eabf3c0d7129a5509279de299f28ee75b98
fejbien <sucholasfejbien@gmail.com>
2022-10-12 11:33:48 +0200

    dodanie

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,15 +12,23 @@
     $DB_USER = "admin";
     $DB_PASS = "123456";
     $DB_NAME = "quiz";
-
     $db = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-    $sql = 
 
-    $res = $db->query($sql);
-    while($row = $res->fetch_assoc()){
-        
+    $sql = "SELECT `id`, `content` FROM `questions` WHERE 1";
+    if($res = $db->query($sqlQusqlestion)){
+        while($row = $res->fetch_assoc()){
+            echo "ID question: ".$row["id"]."<br>";
+            echo "Content: ".$row["content"]."<br><br>";
+        }
     }
 
+    $sql = "SELECT `id`, `content` FROM `answers` WHERE 1";
+    if($res = $db->query($sqlQusqlestion)){
+        while($row = $res->fetch_assoc()){
+            echo "ID answer: ".$row["id"]."<br>";
+            echo "Content: ".$row["content"]."<br><br>";
+        }
+    }
     ?>
 </body>
 </html>
